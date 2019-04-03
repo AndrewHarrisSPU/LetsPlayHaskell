8dbbe191bc340d2788a15c7c1bf049d011a9cb58
Andrew Harris <harrisa15@spu.edu>
2019-04-03 11:28:34 -0700

    Edited w2-2 for clarity, sequencing of the concept of ordering 2 elements, to finding most-ordered element in list, to sorting a list.

--- week2-2.hs
+++ week2-2.hs
@@ -13,28 +13,33 @@ orderOp :: ( Ord a ) => a -> a -> Bool
 orderOp a b =
   a < b
 
-newtype Word = Word String
-
 -- Next: Assigned functions. Straightforward.
 sameStr :: String -> String -> Bool
 sameStr a b =
   a == b
 
-whosFirst :: String -> String -> String
+-- Sorting, functional
+-- reinventing:
+-- minimum [ a, b ]
+whosFirst :: ( Ord a ) => a -> a -> a
 whosFirst a b
   | a < b = a
   | otherwise = b
 
--- the better thing would be to use the function 'minimum', but for practice:
-whosFirstInList :: [ String ] -> String -> String
-whosFirstInList [] min = min
-whosFirstInList ( x : xs ) "" = whosFirstInList xs x
-whosFirstInList ( x : xs ) min = whosFirstInList xs ( whosFirst x min )
+-- reinventing:
+-- minimum [ x, xs ... ]
+whosFirstInList :: ( Ord a ) => [ a ] -> [ a ] -> a
+whosFirstInList [] [ min ] = min
+whosFirstInList ( x : xs ) [] = whosFirstInList xs [ x ]
+whosFirstInList ( x : xs ) [ min ] = whosFirstInList xs [( whosFirst x min )]
 
--- really this is about 3 lines, only one interesting line, to do a sort.
-whosInOrder :: [ String ] -> [ String ]
+-- reinventing:
+-- sort (badly)
+-- Data.List's delete
+-- note: filtering out all instances of the minimum, so this de-dupes as well ...
+whosInOrder :: ( Ord a ) => [ a ] -> [ a ]
 whosInOrder [] = []
-whosInOrder xs = [ whosFirstInList xs "" ] ++ whosInOrder ( filter ( /= whosFirstInList xs "" ) xs )
+whosInOrder xs = [ whosFirstInList xs [] ] ++ whosInOrder ( filter ( /= whosFirstInList xs [] ) xs )
 
 main = do
   let sortedHelloWorld = whosInOrder [ "Hello", "World", "What's", "Up", "?" ]
@@ -50,7 +55,7 @@ main = do
   putStrLn( expect "a" "a" ( minimum [ "b", "a" ]))
 
   let myList = [ "Last", "Middle", "First" ]
-  putStrLn( expect "First in list" "First" ( whosFirstInList myList "" ))
+  putStrLn( expect "First in list" "First" ( whosFirstInList myList [] ))
 
   let myList2 = [ "3", "2", "1" ]
   putStrLn( expect "Sorted" [ "1", "2", "3" ] ( whosInOrder myList2 ))
